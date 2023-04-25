3697357d3f9d54ecb9a6501248c3ce0b024f7283
bhadreshkumarghevariya <bhadreshkumarghevariya@gmail.com>
2023-04-25 08:46:13 -0400

    First Change

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Welcome to My First AWS React Project
         </a>
       </header>
     </div>
